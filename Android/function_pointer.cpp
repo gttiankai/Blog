35bf170fe6042a4a238b648d602b3fe234235d1f
gttiankai <gttiankai@gmail.com>
2014-10-15 16:17:04 +0800

    add function-ponter.cpp 说明函数指针的使用

---
+++ Android/function_pointer.cpp
@@ -0,0 +1,19 @@
+/* 该段代码的作用就是学习一下函数指针的使用,个人感觉函数指针还是非常强大的,
+ * 在某些情况下,只有函数指针才能够解决问题.
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+/* 本例子是说明使用函数指针作为返回值
+ *
+ *
+ *
+ */
+#include <iostream>
+
+float add(float a, float b){return a + b };
+float minns(float a, float b   )
