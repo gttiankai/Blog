8b4d35d803d5cc278abc3a5be34ef29b1c7a2c20
Tian Kai <gttiankai@gmail.com>
2014-10-19 21:44:57 +0800

    add some knowloge in pro-git.org

--- Android/Helloworld.cpp
+++ Android/Helloworld.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+<<<<<<< HEAD
 void Helloworld(){
     std::cout<<"Helloworld construct "<<std::endl;
 }
 void helloprintf(){
     std::cout<<"Hellodlopen"<<std::endl; 
+=======
+
+class Helloworld
+{
+public:
+    Helloworld();
+    virtual ~Helloworld();
+    void printf(){
+        std::cout<<"hello dlopen!"<<std::endl;
+    }
+};
+int main(int argc, char *argv[])
+{
+    Helloworld hello;
+    hello.printf();
+    return 0;
 }
